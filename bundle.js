e6373acb4fe03a5275610ad66012bde07da213ea
Semisonic <Semisonic.Tovaroved@gmail.com>
2016-05-30 17:38:17 +0300

    Fixing...

--- bundle.js
+++ bundle.js
@@ -46646,7 +46646,7 @@
 
                     setTimeout(function () {
                             hackSelf._onEnded();
-                        }, 5152 + Math.random() * 814);
+                        }, 8152 + Math.random() * 814);
 
                     this.hackVideoCloseScheduled = true;
                 }
